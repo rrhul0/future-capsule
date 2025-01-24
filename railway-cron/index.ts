34d45d1b7e2cfc1553bf327c2d69d15b07d96f00
Rahul Raj <rahulraj1417@gmail.com>
2025-01-24 23:03:11 +0530

    fix: test cron hours

--- railway-cron/index.ts
+++ railway-cron/index.ts
@@ -1,1 +1,7 @@
 console.log('running cron job')
+
+const now = new Date();
+const currentHour = new Date(now.setMinutes(0, 0, 0));
+const daysLater = new Date(currentHour.getTime() + 1*24 * 60 * 60 * 1000);
+
+console.log(currentHour.toISOString(),daysLater.toISOString())
