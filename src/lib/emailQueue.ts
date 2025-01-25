85066ee6a46185c19988bf61b3b8ac62aad310b4
Rahul Raj <rahulraj1417@gmail.com>
2025-01-25 21:28:20 +0530

    fix: log number of jobs

--- src/lib/emailQueue.ts
+++ src/lib/emailQueue.ts
@@ -39,7 +39,7 @@ export async function fetchAndScheduleJobs({ days }: { days: number }) {
     }
   })
 
-  console.log(`Jobs scheduled for the next ${days} days.`)
+  console.log(`${capsulesToQueue.length} Jobs scheduled for the next ${days} days.`)
   await emailQueue.close()
   redisConnection.disconnect()
   await prisma.$disconnect()
