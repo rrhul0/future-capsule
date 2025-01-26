457be03d41220aa331e5370eec16b632813e60ab
Rahul Raj <rahulraj1417@gmail.com>
2025-01-26 12:59:59 +0530

    feat: used mantine forms for create capsule form

--- src/components/createCapsuleModal.tsx
+++ src/components/createCapsuleModal.tsx
@@ -6,38 +6,62 @@ import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 import duration from 'dayjs/plugin/duration'
+import { useForm } from '@mantine/form'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.extend(duration)
 
+type FormvaluesType = {
+  emails: string[]
+  message: string
+  openDate?: DateValue
+  openTime?: string
+}
+
 const CreateCapsuleModal = ({ opened, close }: { opened: boolean; close: () => void }) => {
-  const [emails, setEmails] = useState<string[]>([])
+  const form = useForm<FormvaluesType>({
+    mode: 'uncontrolled',
+    initialValues: {
+      emails: [],
+      message: '',
+      openTime: '10:00'
+    },
+    validate: {
+      emails: (value) => (value.length ? null : 'Emails are required'),
+      message: (value) => (value ? null : 'Message is required'),
+      openDate: (value) => (value ? null : 'Open date is required'),
+      openTime: (value) => (value ? null : 'Open time is required')
+    }
+  })
+
   const [multiSelectData, setMultiSelectData] = useState<string[]>()
-  const [message, setMessage] = useState('')
-  const [openDate, setOpenDate] = useState<DateValue>()
   const [minTime, setMinTime] = useState<string | undefined>(undefined)
-  const [openTime, setOpenTime] = useState<string>(minTime || '10:00')
 
   const onChangeSearch = (value: string) => {
+    const emails = form.getValues().emails
     if (!value) setMultiSelectData(emails)
     else setMultiSelectData([...emails, value])
   }
 
   const onDateChange = (date: DateValue) => {
-    setOpenDate(date)
+    form.setValues({ openDate: date })
     if (date) {
       const openDatems = dayjs.tz(date.toISOString()).tz('UTC')
       const diffInDays = openDatems.diff(dayjs().add(5, 'minutes'), 'day', true) // get diff in days from now (+ 5 mins)
-      const minTime = 0 < diffInDays && diffInDays < 1 ? dayjs.duration(1 - diffInDays, 'day') : undefined
+      const minTimeDuration = 0 < diffInDays && diffInDays < 1 ? dayjs.duration(1 - diffInDays, 'day') : undefined
 
-      setMinTime(minTime?.format('HH:mm'))
-      if (minTime) setOpenTime(minTime.format('HH:mm'))
+      setMinTime(minTimeDuration?.format('HH:mm'))
+      if (minTimeDuration) {
+        form.setValues({ openTime: minTimeDuration.format('HH:mm') })
+      }
     }
   }
 
   const action = () => {
-    if (openDate && openTime && emails.length && message) {
+    form.validate()
+    if (form.isValid()) {
+      const { openDate, openTime, emails, message } = form.getValues()
       const date = dayjs(openDate).tz('UTC').format('YYYY-MM-DD')
       const openTimeStamp = dayjs.tz(`${date} ${openTime}`).tz('UTC').format('YYYY-MM-DDTHH:mm:ss')
       createCapsuleAction({ emails, message, timestamp: openTimeStamp }).then(close)
@@ -49,42 +73,36 @@ const CreateCapsuleModal = ({ opened, close }: { opened: boolean; close: () => v
       <form action={action} method='post'>
         <MultiSelect
           data={multiSelectData}
-          value={emails}
+          {...form.getInputProps('emails')}
           onChange={(values) => {
             if (values.at(-1)?.match(/^[^@]+@[^@]+\.[^@]+$/)) {
-              setEmails(values)
+              form.setValues({ emails: values })
               setMultiSelectData(values)
             }
           }}
           searchable
           onRemove={(value) => {
-            setEmails(emails.filter((email) => email !== value))
+            form.setValues({ emails: form.getValues().emails.filter((email) => email !== value) })
           }}
           onSearchChange={onChangeSearch}
           name='emails'
         />
-        <Textarea
-          label='Capsule message'
-          name='message'
-          value={message}
-          onChange={(e) => setMessage(e.currentTarget.value)}
-        />
+        <Textarea label='Capsule message' name='message' {...form.getInputProps('message')} />
         <input type='hidden' name='tzOffset' defaultValue={new Date().getTimezoneOffset().toString()} />
-        <Flex>
+        <Flex gap={10}>
           <DateInput
             label='Open date'
             name='openDate'
             minDate={new Date(new Date().setDate(new Date().getDate() + 1))}
             className='basis-2/3'
-            value={openDate}
+            {...form.getInputProps('openDate')}
             onChange={onDateChange}
           />
           <TimeInput
             label='Open time'
             name='openTime'
             minTime={minTime}
-            value={openTime}
-            onChange={(e) => setOpenTime(e.currentTarget.value)}
+            {...form.getInputProps('openTime')}
             onClick={(e) => e.currentTarget.showPicker()}
             className='cursor-pointer basis-1/3'
           />
