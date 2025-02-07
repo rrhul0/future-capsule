7c4555e1897bb04c15f69d43ccfa899ced842688
Rahul Raj <rahulraj1417@gmail.com>
2025-02-07 23:42:36 +0530

    feat: added CI/CD by github workflow to own selfhosted vps

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis'
 
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379'
+const redisUrl = process.env.REDIS_URL!
 
 export const redisConnection = new Redis(redisUrl + '?family=0', {
   maxRetriesPerRequest: null
